6d094fefdc79f2a78f6b3b5a44a279cc9fd97f64
Swarnali98 <68122050+Swarnali98@users.noreply.github.com>
2020-07-12 01:21:22 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Inverse of matrix
-## Functions
+## Two functions will be created
 
-## Creates a special matric that can cache its inverse
+##"makeCacheMatrix" Creates a special matric that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 j <- NULL
@@ -10,12 +10,20 @@ j <- NULL
                 j <<- NULL
 }
 get <- function()x
-        setInverse <- function(inverse) j <<- inverse
-        getInverse <- function()j
-        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+        setInverse <- function(inverse) j <<- inverse  ##set the value of the matrix
+        getInverse <- function()j                      ##get the value of the matrix
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)   ##set the value of the inverse (setInverse) and to get the value of the inverse (getInverse)
         }
 
 ## Computes the inverse of the matrix made by makeCacheMatrix
+## `cacheSolve`: This function computes the inverse of the special
+##  "matrix" returned by `makeCacheMatrix` above. If the inverse has
+##  already been calculated (and the matrix has not changed), then
+##  `cacheSolve` should retrieve the inverse from the cache.
+##  `cacheSolve` should retrieve the inverse from the cache with the
+##  message "getting cached date" .  Inverse is calculated using function
+##  'solve()'
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
